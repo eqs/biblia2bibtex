29b8d5448d0b43f890c36fa7306285cb6c53d7c4
eqs <tikuwatabetai@gmail.com>
2017-02-05 03:35:48 +0900

    implement conversion biblia2bibtex

--- application.py
+++ application.py
@@ -12,15 +12,42 @@ import datetime
 import flask 
 import pandas as pd
 
+# template string for bibtex
+bibtex_template = """
+@book{{{ISBN}, <br/>
+\tauthor=\"{author}\", <br/>
+\ttitle=\"{title}\", <br/>
+\tpublisher=\"{publisher}\", <br/>
+\tyear=\"{year}\"<br/>
+}}<br/>
+<br/>
+"""
+
 # create flask app
 app = flask.Flask(__name__)
 
-@app.route('/upload', methods=['GET', 'POST'])
+def to_bibtex(table):
+    """表をbibtexに変換する関数
+    :param table: 本の表
+    :return: bibtex化した文字列
+    """
+    text = ""
+    for k, row in table.iterrows():
+        text += bibtex_template.format(
+            ISBN=row['ISBN'], 
+            author=row['Author'].replace("/",  " and "), 
+            title=row['Title'], 
+            publisher=row['Publisher'], 
+            year=''
+        )
+    return text
+
+@app.route('/bibtex.bib', methods=['GET', 'POST'])
 def upload_file():
     # Get file pointer of uploaded file
     f = flask.request.files['file']
     # Convert CSV text to HTML
-    table = pd.io.parsers.read_csv(f)
+    table = pd.io.parsers.read_csv(f, dtype=str, keep_default_na=False, na_values=['***'])
     
     # Drop empty columns
     table.columns = ['Title', '???', 'Author', '???', 'Publisher', 'ISBN', 'Date 1', '???', '???', 'Thumbnail URL', 'Shopping URL', 'Date 2', '???', '???']
@@ -28,7 +55,7 @@ def upload_file():
 
     table['ISBN'] = table['ISBN'].astype(str)
 
-    return table.to_html()
+    return to_bibtex(table)
 
 @app.route('/')
 def hello():
